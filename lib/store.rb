0489c3d0786b4bf0096a1012e3a51337a3f5450d
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-23 03:07:51 +0000

    Destroy empty store, before destroy check the employee availibility

--- lib/store.rb
+++ lib/store.rb
@@ -3,11 +3,20 @@ class Store < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3 }
   validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
   validate :must_include_at_least_one_apparel
+  before_destroy :check_employee_size
 
   def must_include_at_least_one_apparel
     if !womens_apparel.present? && !mens_apparel.present?
       errors.add(:womens_apparel, "must include at least one apparel") 
       errors.add(:mens_apparel, "must include at least one apparel")
+    end
+  end
+
+  def check_employee_size
+
+    if employees.size > 0
+      errors.add(:base,"have employee, can not be destroy")
+      throw :abort
     end
   end
 

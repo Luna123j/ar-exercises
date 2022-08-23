ac0a31565e9558b6af4cfd845eb975780ea50574
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-23 02:32:21 +0000

    add random password before create

--- lib/employee.rb
+++ lib/employee.rb
@@ -5,4 +5,11 @@ class Employee < ActiveRecord::Base
   validates :hourly_rate, numericality: { only_integer: true, greater_than: 40, less_than: 200 }
   validates_associated :store
 
+  before_create :set_password
+
+  private
+    def set_password
+      random_password = Array.new(8){[*"A".."Z", *"0".."9"].sample}.join
+      self.password = random_password
+    end
 end

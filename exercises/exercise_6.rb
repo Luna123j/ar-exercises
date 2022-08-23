3b2b89e477bde15e19322dbf0b61392140ca0e76
Shenglun Jin <sunny.jsl.7@gmail.com>
2022-08-23 02:13:46 +0000

    create employees, add validation, output error

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,10 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Sheng", last_name: "Jin", hourly_rate: 80)
+@store2.employees.create(first_name: "Joy", last_name: "Huang", hourly_rate: 60)
+@store2.employees.create(first_name: "Ritta", last_name: "Zhou", hourly_rate: 70)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validate :must_include_at_least_one_apparel
+
+  def must_include_at_least_one_apparel
+    if !womens_apparel.present? && !mens_apparel.present?
+      errors.add(:womens_apparel, "must include at least one apparel") 
+      errors.add(:mens_apparel, "must include at least one apparel")
+    end
+  end
 
 end
